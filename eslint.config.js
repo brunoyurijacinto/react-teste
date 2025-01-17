2c764c418b55abcde25ec1a5ebba05756705b546
Bruno Yuri <bruno.orle@gmail.com>
2025-01-16 21:29:25 -0300

    Check husky

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,6 @@ export default [
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
-    extends: ['airbnb', 'airbnb/hooks'],
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
